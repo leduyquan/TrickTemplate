687b3039b3499e69465865f05f31acb5793fca8a
LeDuyQuan <quanld.uit@gmail.com>
2020-05-19 15:41:13 +0700

    Update Convert timezone.js

--- Convert timezone.js
+++ Convert timezone.js
@@ -1,9 +1,7 @@
 // Convert utc to local time
-
 localTimeConversion: function(utcTime, timezone, format = `${moment.HTML5_FMT.DATE} ${moment.HTML5_FMT.TIME_SECONDS}`) {
     return moment.utc(utcTime).tz(timezone).format(format);
 },
-
 
 // Convert local to utc time
 formatDatetimeUTC: function(localTime, timezone, format = `${moment.HTML5_FMT.DATE} ${moment.HTML5_FMT.TIME_SECONDS}`) {
